6f99d2a7bf33f9662d9787b8995182bfe00c7ba5
taivo.nurk <taivo.nurk@khk.ee>
2020-12-16 12:55:26 +0200

    modified plugin `Very First Plugin` version 1.0

---
+++ wp-content/plugins/very-first-plugin/very-first-plugin.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+* Plugin Name: Very First Plugin
+* Plugin URI: http://taivonurk.ikt.khk.ee/
+* Description: This is the very first plugin I ever created.
+* Version: 1.0
+* Author: Taivo Nurk
+* Author URI: http://taivonurk.ikt.khk.ee/
+**/
+
+function dh_modify_read_more_link() {
+ return '<a class="more-link" href="' . get_permalink() . '">Click to Read!</a>';
+}
+add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
+
+?>
